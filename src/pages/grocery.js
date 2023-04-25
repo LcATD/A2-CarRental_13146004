6d5c5dfaf44a9a0fe91c993a779cfbd89f7f112c
LcATD <499613077@qq.com>
2023-04-26 05:20:29 +1000

    categorised items correctly

--- src/pages/grocery.js
+++ src/pages/grocery.js
@@ -10,11 +10,61 @@ export function Grocery() {
         Beverage
       </h3>
       <Row md={2} xs={1} lg={3}>
-        {items.map((item) => (
-          <Col>
-            <StoreItem {...item} />
-          </Col>
-        ))}
+        {items
+          .filter((item) => item.subCategory === "Beverage")
+          .map((item) => (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          ))}
+      </Row>
+      <h3 class="body" style={{ marginTop: "-3rem", marginBottom: "2rem" }}>
+        Snack
+      </h3>
+      <Row md={2} xs={1} lg={3}>
+        {items
+          .filter((item) => item.subCategory === "Snack")
+          .map((item) => (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          ))}
+      </Row>
+      <h3 class="body" style={{ marginTop: "-3rem", marginBottom: "2rem" }}>
+        Condiment
+      </h3>
+      <Row md={2} xs={1} lg={3}>
+        {items
+          .filter((item) => item.subCategory === "Condiment")
+          .map((item) => (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          ))}
+      </Row>
+      <h3 class="body" style={{ marginTop: "-3rem", marginBottom: "2rem" }}>
+        Pet
+      </h3>
+      <Row md={2} xs={1} lg={3}>
+        {items
+          .filter((item) => item.subCategory === "Pet")
+          .map((item) => (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          ))}
+      </Row>
+      <h3 class="body" style={{ marginTop: "-3rem", marginBottom: "2rem" }}>
+        Laundry
+      </h3>
+      <Row md={2} xs={1} lg={3}>
+        {items
+          .filter((item) => item.subCategory === "Laundry")
+          .map((item) => (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          ))}
       </Row>
     </>
   );
