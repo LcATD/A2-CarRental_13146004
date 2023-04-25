e08d8e711f0e05c228c9f3215e2a92a40fdf602e
LcATD <499613077@qq.com>
2023-04-25 17:13:54 +1000

    Navbar working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" width="80" height="40" />™
         </a>
+        {/* Nav Bar */}
+        <nav class="navbar">
+          <div class="dropdown">
+            <div class="dropdown-button">Grocery</div>
+            <div class="dropdown-items">
+              <a href="#beverage">Beverage</a>
+              <a href="#snack">Snack</a>
+              <a href="#condiment">Condiment</a>
+              <a href="#pet">Pet</a>
+              <a href="#laundry">Laundry</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <div class="dropdown-button">Fresh</div>
+            <div class="dropdown-items">
+              <a href="#vegetable">Vegetable</a>
+              <a href="#fruit">Fruit</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <div class="dropdown-button">Meat</div>
+            <div class="dropdown-items">
+              <a href="#poultry">Poultry</a>
+              <a href="#beef">Beef</a>
+              <a href="#pork">Pork</a>
+              <a href="#vegan">Vegan</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <div class="dropdown-button">Dairy</div>
+            <div class="dropdown-items">
+              <a href="#milk">Milk</a>
+              <a href="#yogurt">Yogurt</a>
+              <a href="#non-dairy">Non-dairy milk</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <div class="dropdown-button">Frozen</div>
+            <div class="dropdown-items">
+              <a href="#frozen-veg">Frozen Vegetable</a>
+              <a href="#frozen-food">Frozen Food</a>
+              <a href="#dessert">Dessert</a>
+            </div>
+          </div>
+        </nav>
+        {/* Search Bar */}
+        <form>
+          <input
+            type="text"
+            placeholder="Search products..."
+            style={{ width: "20rem", height: "2rem" }}
+          />
+        </form>
       </header>
     </div>
   );
---
+++ src/groceryData/groceryData.js
@@ -0,0 +1,12 @@
+const GroceryData = [
+  {
+    name: "Big Bottled Water",
+    price: 0.9,
+    category: "Grocery",
+    amount: 940,
+    inStock: true,
+    link: "plaeholder URL",
+  },
+];
+
+export default GroceryData;
